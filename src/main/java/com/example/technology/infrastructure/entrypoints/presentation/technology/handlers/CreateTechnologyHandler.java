0a3444136a33a59998ab216dbdf63965b8c4d004
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-07-06 16:04:55 -0500

    feat: implement create technology in handler

--- src/main/java/com/example/technology/infrastructure/entrypoints/presentation/technology/handlers/CreateTechnologyHandler.java
+++ src/main/java/com/example/technology/infrastructure/entrypoints/presentation/technology/handlers/CreateTechnologyHandler.java
@@ -1,9 +1,17 @@
 package com.example.technology.infrastructure.entrypoints.presentation.technology.handlers;
 
 import com.example.technology.domain.api.ITechnologyServicePort;
+import com.example.technology.infrastructure.entrypoints.dto.common.ErrorResponseDTO;
+import com.example.technology.infrastructure.entrypoints.dto.technology.CreateTechnologyDto;
+import com.example.technology.infrastructure.entrypoints.dto.technology.TechnologyDto;
 import com.example.technology.infrastructure.entrypoints.handler.IRouteHandler;
 import com.example.technology.infrastructure.entrypoints.mapper.ITechnologyDtoMapper;
 import com.example.technology.infrastructure.entrypoints.validation.dto.IDtoValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,7 +29,31 @@ public class CreateTechnologyHandler implements IRouteHandler {
     private final IDtoValidator dtoValidator;
 
     @Override
+    @Operation(
+            tags = {"Technology API"},
+            summary = "Create a technology",
+            description = "Allow register a technology",
+            requestBody = @RequestBody(
+                    description = "Technology data",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = CreateTechnologyDto.class))
+            )
+    )
+    @ApiResponse(responseCode = "200", description = "Technology registered successfully",
+            content = @Content(schema = @Schema(implementation = TechnologyDto.class)))
+    @ApiResponse(responseCode = "409", description = "Conflict error",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "422", description = "Business rule error or validation error",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public Mono<ServerResponse> handle(ServerRequest request) {
-        return Mono.empty();
+        return request
+                .bodyToMono(CreateTechnologyDto.class)
+                .flatMap(dtoValidator::validate)
+                .map(technologyDtoMapper::fromCreateToModel)
+                .flatMap(technologyServicePort::createTechnology)
+                .map(technologyDtoMapper::fromModelToResponse)
+                .flatMap(technologyDto -> ServerResponse.ok().bodyValue(technologyDto));
     }
 }
