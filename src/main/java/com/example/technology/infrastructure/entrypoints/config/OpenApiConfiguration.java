2e60fc2d602b09cd1833762c0a3f074ba04f9255
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-07-07 00:47:14 -0500

    fix: add open api swagger config

---
+++ src/main/java/com/example/technology/infrastructure/entrypoints/config/OpenApiConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.technology.infrastructure.entrypoints.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Technology API")
+                        .version("1.0.0")
+                        .description("Reactive challenge microservice")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                );
+    }
+}
