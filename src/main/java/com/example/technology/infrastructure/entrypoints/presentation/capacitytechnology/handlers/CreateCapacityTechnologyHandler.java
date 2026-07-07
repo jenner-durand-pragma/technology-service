a6db0f1231f42dd799353c4920050a23b5b7ddbd
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-07-07 16:54:52 -0500

    feat: implement associate capacity and its technologies in handler

--- src/main/java/com/example/technology/infrastructure/entrypoints/presentation/capacitytechnology/handlers/CreateCapacityTechnologyHandler.java
+++ src/main/java/com/example/technology/infrastructure/entrypoints/presentation/capacitytechnology/handlers/CreateCapacityTechnologyHandler.java
@@ -1,9 +1,18 @@
 package com.example.technology.infrastructure.entrypoints.presentation.capacitytechnology.handlers;
 
 import com.example.technology.domain.api.ICapacityTechnologyServicePort;
+import com.example.technology.infrastructure.entrypoints.dto.capacitytechnology.CreateCapacityTechnologyDto;
+import com.example.technology.infrastructure.entrypoints.dto.common.ErrorResponseDTO;
+import com.example.technology.infrastructure.entrypoints.dto.technology.TechnologyDto;
+import com.example.technology.infrastructure.entrypoints.exception.common.BodyRequiredException;
 import com.example.technology.infrastructure.entrypoints.handler.IRouteHandler;
 import com.example.technology.infrastructure.entrypoints.mapper.ICapacityTechnologyDtoMapper;
 import com.example.technology.infrastructure.entrypoints.validation.dto.IDtoValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,7 +30,31 @@ public class CreateCapacityTechnologyHandler implements IRouteHandler {
     private final IDtoValidator dtoValidator;
 
     @Override
+    @Operation(
+            tags = {"Capacity Technology API"},
+            summary = "Associate capacity with its technologies",
+            description = "Allow register an association between a capacity and its technologies",
+            requestBody = @RequestBody(
+                    description = "Capacity Technology data",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = CreateCapacityTechnologyDto.class))
+            )
+    )
+    @ApiResponse(responseCode = "200", description = "Association between capacity and its technologies saved successfully",
+            content = @Content(schema = @Schema(implementation = TechnologyDto.class)))
+    @ApiResponse(responseCode = "404", description = "A technology was not found",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "422", description = "Business rule error or validation error",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public Mono<ServerResponse> handle(ServerRequest request) {
-        return Mono.empty();
+        return request
+                .bodyToMono(CreateCapacityTechnologyDto.class)
+                .switchIfEmpty(Mono.error(new BodyRequiredException()))
+                .flatMap(dtoValidator::validate)
+                .map(capacityTechnologyDtoMapper::toModel)
+                .flatMap(capacityTechnologyServicePort::assignTechnologiesToCapacities)
+                .then(ServerResponse.noContent().build());
     }
 }
