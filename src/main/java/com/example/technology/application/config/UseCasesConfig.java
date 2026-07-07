3669f6dd1d06b5a80e52ccf57be1527c7fa27594
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-07-07 16:20:17 -0500

    feat: add dependency injection to service and provider adapters

--- src/main/java/com/example/technology/application/config/UseCasesConfig.java
+++ src/main/java/com/example/technology/application/config/UseCasesConfig.java
@@ -1,21 +1,41 @@
 package com.example.technology.application.config;
 
+import com.example.technology.domain.api.ICapacityTechnologyServicePort;
 import com.example.technology.domain.api.ITechnologyServicePort;
+import com.example.technology.domain.spi.ICapacityTechnologyPersistencePort;
 import com.example.technology.domain.spi.ITechnologyPersistencePort;
+import com.example.technology.domain.spi.ITransactionalPersistencePort;
+import com.example.technology.domain.usecase.CapacityTechnologyUseCase;
 import com.example.technology.domain.usecase.TechnologyUseCase;
+import com.example.technology.infrastructure.adapters.persistenceadapter.CapacityTechnologyPersistenceAdapter;
 import com.example.technology.infrastructure.adapters.persistenceadapter.TechnologyPersistenceAdapter;
+import com.example.technology.infrastructure.adapters.persistenceadapter.mapper.ICapacityTechnologyEntityMapper;
 import com.example.technology.infrastructure.adapters.persistenceadapter.mapper.ITechnologyEntityMapper;
+import com.example.technology.infrastructure.adapters.persistenceadapter.repository.ICapacityTechnologyEntityRepository;
 import com.example.technology.infrastructure.adapters.persistenceadapter.repository.ITechnologyEntityRepository;
+import com.example.technology.infrastructure.adapters.transactionalgateway.impl.TransactionalGatewayImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Configuration
 @RequiredArgsConstructor
 public class UseCasesConfig {
+        private final TransactionalOperator transactionalOperator;
 
         private final ITechnologyEntityRepository technologyEntityRepository;
         private final ITechnologyEntityMapper technologyEntityMapper;
+
+        private final ICapacityTechnologyEntityRepository capacityTechnologyEntityRepository;
+        private final ICapacityTechnologyEntityMapper capacityTechnologyEntityMapper;
+
+        @Bean
+        public ITransactionalPersistencePort transactionalPersistencePort() {
+                return new TransactionalGatewayImpl(
+                        transactionalOperator
+                );
+        }
 
         @Bean
         public ITechnologyPersistencePort technologyPersistencePort() {
@@ -30,5 +50,26 @@ public class UseCasesConfig {
                 ITechnologyPersistencePort technologyPersistencePort
         ) {
                 return new TechnologyUseCase(technologyPersistencePort);
+        }
+
+        @Bean
+        public ICapacityTechnologyPersistencePort capacityTechnologyPersistencePort() {
+                return new CapacityTechnologyPersistenceAdapter(
+                        capacityTechnologyEntityRepository,
+                        capacityTechnologyEntityMapper
+                );
+        }
+
+        @Bean
+        public ICapacityTechnologyServicePort capacityTechnologyServicePort(
+                ITechnologyPersistencePort technologyPersistencePort,
+                ICapacityTechnologyPersistencePort capacityTechnologyServicePort,
+                ITransactionalPersistencePort transactionalPersistencePort
+        ) {
+                return new CapacityTechnologyUseCase(
+                        technologyPersistencePort,
+                        capacityTechnologyServicePort,
+                        transactionalPersistencePort
+                );
         }
 }
